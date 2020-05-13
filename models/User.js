20c008896fff34c99e4278147b3e743c94fe8a8e
FanyMarin <nicte-ha.marin@hotmail.com>
2020-05-12 20:35:05 -0500

    validacion del email para user.js agregada

--- models/User.js
+++ models/User.js
@@ -1,16 +1,26 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const userSchema = new Schema(
-    {
-        email: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: [true, "Debes agregar un contraseña"],
-        },
-    }
-)
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Debes agregar un nombre"],
+  },
+  email: {
+    type: String,
+    required: [true, "Debes agregar un email"],
+    validate: {
+      message: "El email ya está en uso",
+      validator: async (email) => {
+        const items = await mongoose.models["User"].count({ email });
+        return items < 1;
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "Debes agregar un contraseña"],
+  },
+});
 
 module.exports = model("User", userSchema);
