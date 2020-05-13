69504f7b86c2587b86790f294d3f6e67e1757456
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-12 22:48:00 -0500

    Add Mongoose connections

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+// Added Mongoose Connections
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/tinderdog-backend", {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then((x) => 
+console.log(`Connected to mongo! database name: "${x.connections[0].name}"`)
+)
+.catch((err) => console.log("Error connecting to the database", err));
 
 app.use(logger('dev'));
 app.use(express.json());
