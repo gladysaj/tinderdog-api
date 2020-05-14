034b632091e094c826a5350c03364d9c3131a8b3
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-13 22:07:52 -0500

    Route to get all foster-dogs by user

--- routes/foster.js
+++ routes/foster.js
@@ -2,6 +2,22 @@ const express = require("express");
 const router = express.Router();
 const Foster = require("../models/Foster");
 
-// Add routes here
+
+// GET all the dogs by user
+router.get("/foster", (req, res) => {
+  const { _id } = req.name;
+  Foster.find({ foster: _id })
+    .populate({
+      path: "dog",
+      populate: {
+        path: "foster",
+        select: "name",
+      },
+    })
+    .then((dogs) => {
+      res.status(200).json({ result: dogs });
+    })
+    .catch((err) => res.status(400).json(err));
+});
 
 module.exports = router;
