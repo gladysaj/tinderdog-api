08201915aa7e0eead4c70b460ffd31b8f08cd6ff
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-14 19:45:36 -0500

    Delete foster routes and add sign up

--- routes/auth.js
+++ routes/auth.js
@@ -3,3 +3,18 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+
+// Sign up
+router.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+  bcrypt.hash(password, 10).then(hashedPassword => {
+    const user =  { email, password: hashedPassword };
+    User.create(user).then(() => {
+      res.status(201).json({ msg: "User was created"});
+    })
+    .catch((err) => res.status(400).json(err));
+  });
+});
+
+
+module.exports = router;
