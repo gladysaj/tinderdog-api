03eb8fee4efc0a485016a7ef801b40433b66719f
Ben Balderas <benbalderas3@gmail.com>
2020-05-23 12:18:34 -0500

    changes in routes

---
+++ public/precache-manifest.075c3db161793c338d1013960442becf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1779c0b8600e47a07a95d43c960e77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896d5cba172bca7366da",
+    "url": "/static/css/main.0570a259.chunk.css"
+  },
+  {
+    "revision": "d5e56222b4b5dadd87a0",
+    "url": "/static/js/2.cf20a44e.chunk.js"
+  },
+  {
+    "revision": "ecc33b8e58b32e46eb82e7d373cc41eb",
+    "url": "/static/js/2.cf20a44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896d5cba172bca7366da",
+    "url": "/static/js/main.6dbde414.chunk.js"
+  },
+  {
+    "revision": "53caa61854ed81ee7dc4",
+    "url": "/static/js/runtime-main.691cca66.js"
+  }
+]);
