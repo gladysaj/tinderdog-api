f2a1abea1bb6f963f9be4a9d4e0d5aae427a0810
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-23 18:13:15 -0500

    Create diislike

--- routes/dislike.js
+++ routes/dislike.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const Dog = require('../models/Dog');
+
 router.post("/dislike", (req, res) => {
-  res.send("dislike")
+  const { id, myDogId } = req.body;
+  Dog.findByIdAndUpdate(myDogId, { $push: { dislikes: id } }, { returnOriginal: false }).then(result => {
+   res.json({ msg: "Disliked" });
+  }).catch(err => {
+    res.status(400).json({ error: err });
+  });
 })
 
 module.exports = router;
