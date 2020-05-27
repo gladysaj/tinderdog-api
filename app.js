741ebea374e12ad3fba528f4b4257f25690d931c
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-27 00:34:52 -0500

    Fix dog routte

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Define routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
-const dogsRouter = require('./routes/create-dog');
+const dogsRouter = require('./routes/dog');
 const likesRouter = require('./routes/like');
 const dislikesRouter = require('./routes/dislike');
 
