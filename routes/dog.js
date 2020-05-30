28dd39f06e1623ff23bee4ae8c25cde1259c449e
FanyMarin <nicte-ha.marin@hotmail.com>
2020-05-29 19:54:50 -0500

    update user and get dog matches routes added

--- routes/dog.js
+++ routes/dog.js
@@ -7,33 +7,41 @@ const { veryToken } = require("../utils/auth");
 router.post("/create-dog", veryToken, (req, res) => {
   const { _id: owner } = req.user;
 
-  Dog.findOne({owner: {$eq: owner}}).then(result => {
-    if(result === null) {
-      Dog.create({ ...req.body, owner })
-      .then((dog) => {
-        res.status(201).json({ result: dog });
-      })
-      .catch((err) => res.status(400).json(err));
-    } else {
-      res.status(401).json({ msg: "ya tienes un perro" })
-    }
-  }).catch(err => res.status(400).json({ error: err }));
+  Dog.findOne({ owner: { $eq: owner } })
+    .then((result) => {
+      if (result === null) {
+        Dog.create({ ...req.body, owner })
+          .then((dog) => {
+            res.status(201).json({ result: dog });
+          })
+          .catch((err) => res.status(400).json(err));
+      } else {
+        res.status(401).json({ msg: "ya tienes un perro" });
+      }
+    })
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 // Request all dogs for match
-router.get('/match', veryToken, (req, res) => {
+router.get("/match", veryToken, (req, res) => {
   const { _id: id } = req.user;
-  
-  Dog.findOne({owner: {$eq: id}}).then(result => {
-    const myDogGender = result.gender;
-    const oppositeGender = myDogGender === "Male" ? "Female" : "Male";
 
-    Dog.find({
-      owner:{$ne: id}, foster:false, gender:oppositeGender
-    }).then(result => {
-      res.send(result);
-    }).catch(err => res.status(400).json({ error: err }));
-  }).catch(err => res.status(400).json({ error: err }));
+  Dog.findOne({ owner: { $eq: id } })
+    .then((result) => {
+      const myDogGender = result.gender;
+      const oppositeGender = myDogGender === "Male" ? "Female" : "Male";
+
+      Dog.find({
+        owner: { $ne: id },
+        foster: false,
+        gender: oppositeGender,
+      })
+        .then((result) => {
+          res.send(result);
+        })
+        .catch((err) => res.status(400).json({ error: err }));
+    })
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 // router.get('/match', veryToken, (req, res) => {
@@ -51,14 +59,17 @@ router.get('/match', veryToken, (req, res) => {
 // });
 
 // Request all dogs for foster
-router.get('/foster', veryToken, (req, res) => {
+router.get("/foster", veryToken, (req, res) => {
   const { _id: id } = req.user;
-    
-    Dog.find({
-      owner:{$ne: id}, foster:true
-    }).then(result => {
+
+  Dog.find({
+    owner: { $ne: id },
+    foster: true,
+  })
+    .then((result) => {
       res.send(result);
-    }).catch(err => res.status(400).json({ error: err }));
+    })
+    .catch((err) => res.status(400).json({ error: err }));
 });
 
 // Update dog
@@ -76,24 +87,41 @@ router.patch("/:id", veryToken, (req, res) => {
 router.post("/only-like", veryToken, (req, res) => {
   const { myDogId, likedDogId } = req.body;
 
-  Dog.findByIdAndUpdate(myDogId, {$push: {liked: likedDogId}}, {new: true}).then(dog => {
-    res.status(200).json({ msg: "Likeado", dog })
-  }).catch(err => res.status(400).json(err))
-})
+  Dog.findByIdAndUpdate(
+    myDogId,
+    { $push: { liked: likedDogId } },
+    { new: true }
+  )
+    .then((dog) => {
+      res.status(200).json({ msg: "Likeado", dog });
+    })
+    .catch((err) => res.status(400).json(err));
+});
 
 // Match
 router.post("/is-match", veryToken, (req, res) => {
   const { myDogId, likedDogId } = req.body;
-  
-  Dog.findByIdAndUpdate(myDogId, {$push: {match: likedDogId}}, {new: true}).then(dog => {
-    
-    Dog.findByIdAndUpdate(likedDogId, {$push: {match: myDogId}}, {new: true}).then(dog => {
-      console.log("Algo pasó")
-    }).catch(err => console.log("Hubo un error"))
 
-    res.status(200).json({ msg: "Tienes un match!", dog })
-  }).catch(err => res.status(400).json(err))
-})
+  Dog.findByIdAndUpdate(
+    myDogId,
+    { $push: { match: likedDogId } },
+    { new: true }
+  )
+    .then((dog) => {
+      Dog.findByIdAndUpdate(
+        likedDogId,
+        { $push: { match: myDogId } },
+        { new: true }
+      )
+        .then((dog) => {
+          console.log("Algo pasó");
+        })
+        .catch((err) => console.log("Hubo un error"));
+
+      res.status(200).json({ msg: "Tienes un match!", dog });
+    })
+    .catch((err) => res.status(400).json(err));
+});
 
 // Delete dog
 router.delete("/:id", veryToken, (req, res) => {
@@ -103,6 +131,17 @@ router.delete("/:id", veryToken, (req, res) => {
       res.status(200).json({
         result: dog,
       });
+    })
+    .catch((err) => res.status(400).json(err));
+});
+
+// Get dog matches
+router.get("/my-matches/:dog_id", veryToken, (req, res) => {
+  const { dog_id } = req.params;
+  Dog.findById(dog_id)
+    .populate("match")
+    .then((matches) => {
+      res.status(200).json({ result: matches });
     })
     .catch((err) => res.status(400).json(err));
 });
